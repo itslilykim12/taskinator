2dd63e9d576f6dfb22d0fb71cf496bcbbeef6af8
nlilu <nlilum.kim@gmail.com>
2021-04-03 12:59:01 -0400

    add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,7 @@ var taskFormHandler = function(event) {
 
   if (isEdit) {
     var taskId = formEl.getAttribute("data-task-id");
+
     completeEditTask(taskNameInput, taskTypeInput, taskId);
   } else {
     var taskDataObj = {
@@ -41,7 +42,8 @@ var taskFormHandler = function(event) {
 var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemE1.setAttribute("data-task-id", taskIdCounter);
+    listItemEl.setAttribute("data-task-Id", taskIdCounter);
+ 
 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
@@ -95,6 +97,22 @@ var createTaskActions = function(taskId) {
   return actionContainerEl;
 };
 
+var completeEditTask = function(taskName, taskType, taskId) {
+  // find task list item with taskId value
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+  // set new values
+  taskSelected.querySelector("h3.task-name").textContent = taskName;
+  taskSelected.querySelector("span.task-type").textContent = taskType;
+
+  alert("Task Updated!");
+
+// remove data attribute from form
+formEl.removeAttribute("data-task-id");
+// update formEl button to go back to saying "Add Task" instead of "Edit Task"
+formEl.querySelector("#save-task").textContent = "Add Task";
+};
+
 var taskButtonHandler = function(event) {
   // get target element from event
   var targetEl = event.target;
@@ -108,6 +126,26 @@ var taskButtonHandler = function(event) {
   else if (targetEl.matches(".delete-btn")) {
     var taskId = targetEl.getAttribute("data-task-id");
     deleteTask(taskId);
+  }
+};
+var taskStatusChangeHandler = function(event) {
+  // get the task item's id
+  var taskId = event.target.getAttribute("data-task-id");
+
+  // get the currently selected option's value and convert to lowercase
+  var statusValue = event.target.value.toLowerCase();
+
+  // find the parent task item element based on the id
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  
+  if (statusValue === "to do") {
+    tasksToDoEl.appendChild(taskSelected);
+  } 
+  else if (statusValue === "in progress") {
+    tasksInProgressEl.appendChild(taskSelected);
+  } 
+  else if (statusValue === "completed") {
+    tasksCompletedEl.appendChild(taskSelected);
   }
 };
 
@@ -136,5 +174,10 @@ var editTask = function(taskId) {
 
 //create a new task button
 formEl.addEventListener("submit", taskFormHandler);
+
 // for edit and delete buttons
-pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentE1.addEventListener("click", taskButtonHandler);
+
+//for changing status
+pageContentE1.addEventListener("change", taskStatusChangeHandler);
+
